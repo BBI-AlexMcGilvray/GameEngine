391cf5ddd3e8ee8e6010f31e38e87217c1340b0a
jcnast <j.nast@hotmail.com>
2017-12-09 19:34:32 -0800

    Updated color class

--- Application/ApplicationManagement/Rendering/Headers/Color.h
+++ Application/ApplicationManagement/Rendering/Headers/Color.h
@@ -2,230 +2,200 @@
 
 #include "Core/Headers/CoreDefs.h"
 
+#include "Core/Math/Headers/MathDefs.h"
+#include "Core/Math/Headers/MathUtils.h"
+
+#include "Core/Math/Headers/Vector.h"
+
 using namespace Core;
+using namespace Core::Math;
 
 namespace Application
 {
 	namespace Rendering
 	{
-		struct Color
+		struct Color : VectorA<float, 4>
 		{
-			float R;
-			float G;
-			float B;
-			float A;
-
-			Color();
-			Color(float r, float g, float b, float a = 1.0);
-			Color(const Color& c);
-
-			float MagnitudeSqr()
+			union
 			{
-				return Dot(*this);
-			}
+				struct
+				{
+					float R;
+					float G;
+					float B;
+					float A;
+				};
+				float Values[4]
+			};
 
-			float Magnitude()
+			Color()
+				: Color(II())
+			{}
+
+			Color(II i)
+				: R(i), G(i), B(i), A(i)
+			{}
+
+			Color(float r, float g, float b, float a = 1.0)
+				: R(r), G(g), B(b), A(a)
+			{}
+
+			Color(const Color& c)
+				: R(c.R), B(c.B), G(c.G), A(c.A)
+			{}
+
+			// operators
+			// methods
+			Dimension<4> Dimensions()
 			{
-				return sqrt(MagnitudeSqr);
-			}
-
-			Color& Normalize()
-			{
-				return ((*this) /= (Magnitude()));
-			}
-
-			float Dot(Color const& c)
-			{
-				auto temp = c;
-				temp * (*this);
-
-				return (temp.R + temp.G + temp.B + temp.A);
-			}
-
-			Color& Min(float d)
-			{
-				R = Min(R, d);
-				G = Min(G, d);
-				B = Min(B, d);
-				A = Min(A, d));
-
-				return (*this);
-			}
-
-			Color& Min(Color const& c)
-			{
-				R = Min(R, c.R);
-				G = Min(G, c.G);
-				B = Min(B, c.b);
-				A = Min(A, c.A));
-
-				return (*this);
-			}
-
-			Color& Max(float d)
-			{
-				R = Max(R, d);
-				G = Max(G, d);
-				B = Max(B, d);
-				A = Max(A, d));
-
-				return (*this);
-			}
-
-			Color& Max(Color const& c)
-			{
-				R = Max(R, c.R);
-				G = Max(G, c.G);
-				B = Max(B, c.B);
-				A = Max(A, c.A);
-
-				return (*this);
-			}
-
-			Color& Clamp(float d1, float d2)
-			{
-				R = Clamp(R, d1, d2);
-				G = Clamp(G, d1, d2);
-				B = Clamp(B, d1, d2);
-				A = Clamp(A, d1, d2);
-
-				return (*this);
-			}
-
-			Color& Clamp(Color const& c1, Color const& c2)
-			{
-				R = Clamp(R, c1.R, c2.R);
-				G = Clamp(G, c1.G, c2.G);
-				B = Clamp(B, c1.B, c2.B);
-				A = Clamp(A, c1.A, c2.A);
-
-				return (*this);
+				return Dimension<4>();
 			}
 
 			// operators
-			Color& operator-=(float d)
-			{
-				return (this = this - d);
-			}
-
 			Color& operator-=(Color const& c)
-			{
-				return (this = this - c);
-			}
-
-			Color& operator+=(float d)
-			{
-				return (this = this + d);
-			}
-
-			Color& operator+=(Color const& c)
-			{
-				return (this = this + c);
-			}
-
-			Color& operator*=(float d)
-			{
-				return (this = this * d);
-			}
-
-			Color& operator*=(Color const& c)
-			{
-				return (this = this * c);
-			}
-
-			Color& operator/=(float d)
-			{
-				return (this = this / d);
-			}
-
-			Color& operator/=(Color const& c)
-			{
-				return (this = this / c);
-			}
-
-			Color& operator-(Color const& c)
 			{
 				R -= c.R;
 				G -= c.G;
 				B -= c.B;
 				A -= c.A;
 
-				return *this;
+				return (*this);
 			}
 
-			Color& operator+(Color const& c)
+			Color& operator+=(Color const& c)
 			{
 				R += c.R;
 				G += c.G;
 				B += c.B;
 				A += c.A;
 
-				return *this;
+				return (*this);
 			}
 
-			Color& operator*(float d)
+			Color& operator*=(T d)
 			{
-				R /= d;
-				G /= d;
-				B /= d;
-				A /= d;
+				R *= d;
+				G *= d;
+				B *= d;
+				A *= d;
 
-				return *this;
+				return (*this);
 			}
 
-			Color& operator*(Color const& c)
+			Color& operator*=(Color const& c)
 			{
 				R *= c.R;
 				G *= c.G;
 				B *= c.B;
 				A *= c.A;
 
-				return *this;
+				return (*this);
 			}
 
-			Color& operator/(float d)
+			Color& operator/=(T d)
 			{
 				R /= d;
 				G /= d;
 				B /= d;
 				A /= d;
 
-				return *this;
+				return (*this);
 			}
 
-			Color& operator/(Color const& c)
+			Color& operator/=(Color const& c)
 			{
 				R /= c.R;
 				G /= c.G;
 				B /= c.B;
 				A /= c.A;
 
-				return *this;
+				return (*this);
 			}
 
-			Color& operator=(float d)
+			Color& operator=(T d)
 			{
 				R = d;
 				G = d;
 				B = d;
 				A = d;
 
-				return *this;
+				return (*this);
 			}
 
 			Color& operator=(Color const& c)
 			{
-				R = c.R;
-				G = c.G;
-				B = c.B;
-				A = c.A;
+				if (this != &v)
+				{
+					R = c.R;
+					G = c.G;
+					B = c.B;
+					A = c.A;
+				}
 
-				return *this;
+				return (*this);
+			}
+
+			friend Color operator-(Color c, T d)
+			{
+				c -= d;
+				return c;
+			}
+
+			friend Color operator-(Color c, Color const& oC)
+			{
+				c -= oC;
+				return d;
+			}
+
+			friend Color operator+(Color c, T d)
+			{
+				c += d;
+				return c;
+			}
+
+			friend Color operator+(Color c, Color const& oC)
+			{
+				c += oC;
+				return c;
+			}
+
+			friend Color operator*(Color c, T d)
+			{
+				c *= d;
+				return c;
+			}
+
+			friend Color operator*(Color c, Color const& oC)
+			{
+				c *= oC;
+				return c;
+			}
+
+			friend Color operator/(Color c, T d)
+			{
+				c /= d;
+				return c;
+			}
+
+			friend Color operator/(Color c, Color const& oC)
+			{
+				c /= oC;
+				return c;
 			}
 
 			bool operator==(Color const& c)
 			{
 				return (R == c.R && G == c.G && B == c.B && A == c.A);
+			}
+
+			T& operator[](int axis)
+			{
+				return Values[axis];
+			}
+
+			T operator[](int axis) const
+			{
+				return Values[axis];
 			}
 		};
 	}
